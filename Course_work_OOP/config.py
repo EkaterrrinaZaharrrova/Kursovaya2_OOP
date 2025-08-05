a3e724c11c1bd0afa793e9b206fb025cd62f3127
Екатерина Захарова <k6orodaenko@yandex.ru>
2025-08-05 14:32:33 +0300

    на проверку

--- Course_work_OOP/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ROOT_DIR = os.path.dirname(__file__)
-LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
-DATA_DIR = os.path.join(ROOT_DIR, 'data/')
-
-PATH_LOGS = os.path.join(LOGS_DIR, 'logs.log')
